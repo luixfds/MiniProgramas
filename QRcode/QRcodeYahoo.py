e40e44768a4e8a0342eb37ba8136b88ada1a5a9c
luixfds <l.fds@yahoo.com>
2021-09-26 13:49:30 -0300

    upando minis

---
+++ QRcode/QRcodeYahoo.py
@@ -0,0 +1,12 @@
+import qrcode
+from qrcode.image.pil import PilImage
+
+features = qrcode.QRCode(version=1,box_size=40,border=3)
+
+features.add_data('https://www.youtube.com/watch?v=qRFPGuBc-KE')
+features.make(fit=True)
+
+generate_image = features.make_image(fill_color="black", back_color="white")
+generate_image.save('video.png')
+
+generate_image.show()
